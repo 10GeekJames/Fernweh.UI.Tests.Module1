cb31ba3e64a6a8dca27d2279e2abd08396a9fbda
JamesK <James@10-geek.com>
2023-10-05 19:45:32 -0500

    updated reports to include step-bindings

--- src/reports.bat
+++ src/reports.bat
@@ -1,7 +1,5 @@
-livingdoc feature-folder ./ -t ./bin/Debug/net7.0/TestExecution* --output ./Reports/TestFeatureReport.html
+livingdoc feature-folder ./ -t ./bin/Debug/net7.0/TestExecution* --output ./Reports/TestFeatureReport.html --binding-assemblies ./../src/bin/Debug/net7.0/Fernweh.BlazorClient.UITests.dll
 
-livingdoc feature-folder ./ --output-type JSON -t ./bin/Debug/net7.0/TestExecution*.json --output ./Reports/TestFeatureData.json
-
-livingdoc test-assembly ./bin/Debug/net7.0/Fernweh.BlazorClient.UITests.dll -t ./Reports/TestFeatureData.json --output ./Reports/TestAssemblyReport.html
+livingdoc feature-folder ./ --output-type JSON -t ./bin/Debug/net7.0/TestExecution*.json --output ./Reports/TestFeatureData.json --binding-assemblies ./../src/bin/Debug/net7.0/Fernweh.BlazorClient.UITests.dll
 
 explorer .\Reports
